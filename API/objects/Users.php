6f9d1b51962d8c0bd5473c06b18c9410c4548898
Tacohontas <Jonathan.undhagen@gmail.com>
2020-03-25 17:34:34 +0100

    add function to register user

--- API/objects/Users.php
+++ API/objects/Users.php
@@ -2,7 +2,8 @@
 include("../../config/database_handler.php");
 
 
-class User{
+class User
+{
     private $database_handler;
     private $username;
     private $token_validity_time = 15; // Minutes
@@ -13,6 +14,154 @@ class User{
     }
 
 
+    public function addUser($username_IN, $password_IN, $email_IN, $roleId_IN)
+    {
+        if ($this->isUsernameTaken($username_IN) === false) {
+
+            if ($this->isEmailTaken($email_IN) === false) {
+
+                // Try to insert User to DB
+                $result = $this->insertUserToDB($username_IN, $password_IN, $email_IN, $roleId_IN);
+
+                if ($result !== false) {
+                    return $result;
+                } else {
+                    $errorMessage = "Couldn't insert User in DB";
+                }
+            } else {
+                $errorMessage = "Email is taken";
+            }
+        } else {
+            $errorMessage = "Username is taken";
+        }
 
 
+
+        $errorLocation = "AddUser in Users.php";
+        return $this->errorHandler($errorMessage, $errorLocation);
+    }
+
+    private function isUsernameTaken($username_IN)
+    {
+        $query_string = "SELECT Username FROM Users WHERE Username = :username_IN";
+
+        $statementHandler = $this->database_handler->prepare($query_string);
+
+        if ($statementHandler !== false) {
+
+            $statementHandler->bindParam(":username_IN", $username_IN);
+            $execSuccess = $statementHandler->execute();
+
+            if ($execSuccess === true) {
+                $result = $statementHandler->fetch(PDO::FETCH_ASSOC);
+
+                print_r($result);
+
+                if (!empty($result)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                $errorMessage = "Execute failed";
+                $errorLocation = "isUsernameTaken in Users.php";
+            }
+        } else {
+            $errorMessage = "Statementhandler failed";
+            $errorLocation = "isUsernameTaken in Users.php";
+        }
+
+        return $this->errorHandler($errorMessage, $errorLocation);
+    }
+
+    private function isEmailTaken($email_IN)
+    {
+        $query_string = "SELECT Email FROM Users WHERE Email = :email_IN";
+
+        $statementHandler = $this->database_handler->prepare($query_string);
+
+        if ($statementHandler !== false) {
+
+            $statementHandler->bindParam(":email_IN", $email_IN);
+            $execSuccess = $statementHandler->execute();
+
+            if ($execSuccess === true) {
+                $result = $statementHandler->fetch(PDO::FETCH_ASSOC);
+
+                if (!empty($result)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                $errorMessage = "Execute failed";
+                $errorLocation = "isEmailTaken in Users.php";
+            }
+        } else {
+            $errorMessage = "Statementhandler failed";
+            $errorLocation = "isEmailTaken in Users.php";
+        }
+
+        return $this->errorHandler($errorMessage, $errorLocation);
+    }
+
+    private function insertUserToDb($username_IN, $password_IN, $email_IN, $roleId_IN)
+    {
+        $query_string = "INSERT INTO Users(Username, Password, Email, Roles_Id) VALUES(:username_IN , :password_IN, :email_IN, :roleId_IN)";
+
+        $statementHandler = $this->database_handler->prepare($query_string);
+
+        if ($statementHandler !== false) {
+
+            $encryptedPassword = md5($password_IN);
+
+            $statementHandler->bindParam(":username_IN", $username_IN);
+            $statementHandler->bindParam(":password_IN", $encryptedPassword);
+            $statementHandler->bindParam(":email_IN", $email_IN);
+            $statementHandler->bindParam(":roleId_IN", $roleId_IN);
+
+            $execSuccess = $statementHandler->execute();
+
+            if ($execSuccess === true) {
+
+                $last_inserted_id = $this->database_handler->lastInsertId();
+                
+                $return = $this->getUser($last_inserted_id);
+                $message = "{$return['Role']} '{$return['Username']}' inserted to DB successfully";
+                return $message;
+            }
+        }
+    }
+
+    private function getUser($userId_IN)
+    {
+        $query_string = "SELECT Users.Id, Username, Email, Date_Created, Name AS Role FROM Users JOIN Roles ON Roles_Id = Roles.Id WHERE Users.Id = :userId_IN";
+
+        $statementHandler = $this->database_handler->prepare($query_string);
+
+        if ($statementHandler !== false) {
+            $statementHandler->bindParam(":userId_IN", $userId_IN);
+            $execSuccess = $statementHandler->execute();
+
+            if ($execSuccess === true) {
+
+                $result = $statementHandler->fetch(PDO::FETCH_ASSOC);
+
+                // Kolla ifall resultatet inte är tomt
+                if(!empty($result)){
+                    return $result;
+                }
+            }
+
+        }
+    }
+
+    private function errorHandler($message_IN, $errorLocation_IN)
+    {
+        $returnObject = new stdClass;
+
+        $returnObject->message = $message_IN;
+        $returnObject->location = $errorLocation_IN;
+        echo json_encode($returnObject);
+    }
 }
--- API/v1/users/add_User.php
+++ API/v1/users/add_User.php
@@ -1,3 +1,40 @@
 <?php
-
 include("../../objects/Users.php");
+
+// test_data
+// $_POST['username'] = "Test";
+// $_POST['password'] = "password";
+// $_POST['email'] = "test@test.se";
+// $_POST['role_id'] = 1;
+
+print_r($_POST);
+
+// Init error
+$error = false;
+$errorMessages = "";
+
+// Check for empty values
+
+if (empty($_POST['username'])) {
+    $error = true;
+    $errorMessages = "Username is empty! ";
+}
+
+if (empty($_POST['password'])) {
+    $error = true;
+    $errorMessages .= "Password is empty! ";
+}
+
+if (empty($_POST['email'])) {
+    $error = true;
+    $errorMessages .= "Email is empty! ";
+}
+
+if($error == true){
+    echo $errorMessages;
+    die;
+}
+
+
+$user_handler = new User($dbh);
+echo $user_handler->addUser($_POST['username'], $_POST['password'], $_POST['email'], $_POST['role_id']);
